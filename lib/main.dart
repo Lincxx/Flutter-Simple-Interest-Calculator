a31ab633c5dc1ada79e73590a97f35e06ddc396b
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-01-22 12:16:12 -0500

    Layout Complete

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: 'Simple Interest Calculator App',
     home: SIForm(),
   ));
@@ -19,13 +20,99 @@ class _SIFormState extends State<SIForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //resizeToAvoidBottomPadding: false,
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
       ),
       body: Container(
         margin: EdgeInsets.all(_minPadding * 10),
-        child: Column(
-          children: <Widget>[getImageAssest()],
+        child: ListView(
+          children: <Widget>[
+            getImageAssest(),
+            Padding( child: TextField(
+              keyboardType: TextInputType.number,
+              decoration: InputDecoration(
+                labelText: 'Principal',
+                hintText: 'Enter Principal e.g. 12000',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5.0)
+                )
+              ),
+            ),
+            padding: EdgeInsets.only(top: _minPadding, bottom: _minPadding),
+            ),
+            Padding(
+              child:TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: 'Rate of Interest',
+                  hintText: 'In percent',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0)
+                  )
+                ),
+              ),
+              padding: EdgeInsets.only(top: _minPadding, bottom: _minPadding),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: _minPadding, bottom: _minPadding),
+              child: Row(
+              children: <Widget>[
+                Expanded(
+                  child:  TextField(
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                      labelText: 'Term',
+                      hintText: 'Time in years',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(5.0)
+                      )
+                    ),
+                  ),
+                ),
+                Container(width: _minPadding * 5),
+                Expanded(
+                  child: DropdownButton<String>(
+                  items: _currencies.map((String value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                      child: Text(value),
+                    );
+                  }).toList(),
+                  value: 'Rupess',
+                  onChanged: (String newValueSelected){
+
+                  }
+                )
+                )
+                
+              ],
+            ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: _minPadding, top: _minPadding),
+              child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: RaisedButton(
+                    child: Text('Calculate'),
+                    onPressed: (){},
+                  ),
+                ),
+                Expanded(
+                  child: RaisedButton(
+                    child: Text('Reset'),
+                    onPressed: (){},
+                  ),
+                )
+              ],
+            ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(bottom: _minPadding, top: _minPadding),
+              child: Text('Todo Text'),
+            )
+          ],
         ),
       ),
     );
